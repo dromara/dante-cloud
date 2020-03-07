2f69388d377d179bed69eb6bb3a3b10c8d8242d9
Felix <pointer_v@qq.com>
2020-03-07 19:05:10 +0800

    优化代码，解决Token过期抛错的问题。

--- eurynome-cloud-services/eurynome-cloud-upms-api/src/main/java/cn/herodotus/eurynome/upms/api/constants/UpmsConstants.java
+++ eurynome-cloud-services/eurynome-cloud-upms-api/src/main/java/cn/herodotus/eurynome/upms/api/constants/UpmsConstants.java
@@ -18,19 +18,19 @@ public class UpmsConstants {
 
     public static final String INDEX_CACHE_NAME = "index:";
 
-    public static final String CACHE_NAME_SYSACCOUNT = "upms:cache:sysaccount:";
+    public static final String CACHE_NAME_SYSACCOUNT = "data:upms:sysaccount:";
     public static final String CACHE_NAME_SYSACCOUNT_INDEX = CACHE_NAME_SYSACCOUNT + INDEX_CACHE_NAME;
 
-    public static final String CACHE_NAME_SYSAPPLICATION = "upms:cache:sysapplication:";
+    public static final String CACHE_NAME_SYSAPPLICATION = "data:upms:sysapplication:";
     public static final String CACHE_NAME_SYSAPPLICATION_INDEX = CACHE_NAME_SYSAPPLICATION + INDEX_CACHE_NAME;
-    public static final String CACHE_NAME_SYSAUTHORITY = "upms:cache:sysauthority:";
+    public static final String CACHE_NAME_SYSAUTHORITY = "data:upms:sysauthority:";
     public static final String CACHE_NAME_SYSAUTHORITY_INDEX = CACHE_NAME_SYSAUTHORITY + INDEX_CACHE_NAME;
-    public static final String CACHE_NAME_SYSROLE = "upms:cache:sysrole:";
+    public static final String CACHE_NAME_SYSROLE = "data:upms:sysrole:";
     public static final String CACHE_NAME_SYSROLE_INDEX = CACHE_NAME_SYSROLE + INDEX_CACHE_NAME;
-    public static final String CACHE_NAME_SYSUSER = "upms:cache:sysuser:";
+    public static final String CACHE_NAME_SYSUSER = "data:upms:sysuser:";
     public static final String CACHE_NAME_SYSUSER_INDEX = CACHE_NAME_SYSUSER + INDEX_CACHE_NAME;
 
-    public static final String CACHE_NAME_OAUTHSCOPE = "upms:cache:oauthscope:";
+    public static final String CACHE_NAME_OAUTHSCOPE = "data:upms:oauthscope:";
     public static final String CACHE_NAME_OAUTHSCOPE_INDEX = CACHE_NAME_OAUTHSCOPE + INDEX_CACHE_NAME;
 
     private static final List<Map<String, Object>> STATUS_ENUM = StatusEnum.getToJsonStruct();
