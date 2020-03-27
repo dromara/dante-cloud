00c447751d9c038b26725486406dcf5ffbd0060e
Felix <pointer_v@qq.com>
2020-03-27 11:29:57 +0800

    1、优化Nacos配置 2、增加Admin服务

---
+++ eurynome-cloud-platform/eurynome-cloud-platform-admin/src/main/java/cn/herodotus/eurynome/admin/AdminApplication.java
@@ -0,0 +1,17 @@
+package cn.herodotus.eurynome.admin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableAdminServer
+public class AdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminApplication.class, args);
+    }
+
+}
