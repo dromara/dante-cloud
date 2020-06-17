4f45d1bf63f2b30159b1eb84638b83c4e0589719
Felix <pointer_v@qq.com>
2020-06-17 17:03:06 +0800

    增加bpmn服务，基于camunda

---
+++ services/eurynome-cloud-bpmn-rest/src/main/java/cn/herodotus/eurynome/bpmn/rest/BpmnApplication.java
@@ -0,0 +1,13 @@
+package cn.herodotus.eurynome.bpmn.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpmnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BpmnApplication.class, args);
+    }
+
+}
