9b2d6f6ce3d846ab8944e9f76d1a601c3de6b94e
herodotus <pointer_v@qq.com>
2021-04-19 08:57:29 +0800

    v2.4.2.50 优化阿里短信代码，补充注释

--- packages/eurynome-cloud-integration/src/main/java/cn/herodotus/eurynome/integration/sms/service/aliyun/AliyunSendSmsService.java
+++ packages/eurynome-cloud-integration/src/main/java/cn/herodotus/eurynome/integration/sms/service/aliyun/AliyunSendSmsService.java
@@ -28,73 +28,95 @@ import java.util.Map;
 @Service
 public class AliyunSendSmsService {
 
-	@Autowired
-	private AliyunProperties aliyunProperties;
-	@Autowired
-	private IAcsClient iAcsClient;
+    @Autowired
+    private AliyunProperties aliyunProperties;
+    @Autowired
+    private IAcsClient iAcsClient;
 
-	public SmsResponse send(List<String> phoneNumbers, Map<String, Object> templateParam) {
-		String phoneNumberString = StringUtils.join(phoneNumbers, ",");
-		String defaultTemplateId = aliyunProperties.getSms().getDefaultTemplateId();
-		if (StringUtils.isBlank(defaultTemplateId)) {
-			log.error("[Eurynome] |- Can not found default templateId in Aliyun sms property, please check!");
-		}
-		return this.send(phoneNumberString, templateParam);
-	}
+    /**
+     * 发送短信 使用默认模版，需要在AliyunProperties中，设置defaultTemplateId属性
+     *
+     * @param phoneNumbers  电话号码
+     * @param templateParam 短信模版中涉及的参数
+     * @return 自定义短信响应实体 SmsResponse {@link SmsResponse}
+     */
+    public SmsResponse send(List<String> phoneNumbers, Map<String, Object> templateParam) {
+        String phoneNumberString = StringUtils.join(phoneNumbers, ",");
+        String defaultTemplateId = aliyunProperties.getSms().getDefaultTemplateId();
+        if (StringUtils.isBlank(defaultTemplateId)) {
+            log.error("[Eurynome] |- Can not found default templateId in Aliyun sms property, please check!");
+        }
+        return this.send(phoneNumberString, templateParam);
+    }
 
-	public SmsResponse send(String phoneNumbers, Map<String, Object> templateParam) {
-		String defaultTemplateId = aliyunProperties.getSms().getDefaultTemplateId();
-		if (StringUtils.isBlank(defaultTemplateId)) {
-			log.error("[Eurynome] |- Can not found default templateId in Aliyun sms property, please check!");
-		}
-		return this.send(phoneNumbers, defaultTemplateId, templateParam);
-	}
+    /**
+     * 发送短信 使用默认模版，需要在AliyunProperties中，设置defaultTemplateId属性
+     *
+     * @param phoneNumbers  电话号码字符串，可以是多个电话号码以英文“,”分割
+     * @param templateParam 短信模版中涉及的参数
+     * @return 自定义短信响应实体 SmsResponse {@link SmsResponse}
+     */
+    public SmsResponse send(String phoneNumbers, Map<String, Object> templateParam) {
+        String defaultTemplateId = aliyunProperties.getSms().getDefaultTemplateId();
+        if (StringUtils.isBlank(defaultTemplateId)) {
+            log.error("[Eurynome] |- Can not found default templateId in Aliyun sms property, please check!");
+        }
+        return this.send(phoneNumbers, defaultTemplateId, templateParam);
+    }
 
-	public SmsResponse send(List<String> phoneNumbers, String templateId, Map<String, Object> templateParam) {
-		String phoneNumberString = StringUtils.join(phoneNumbers, ",");
-		return this.send(phoneNumberString, templateId, templateParam);
-	}
+    /**
+     * 发送短信
+     *
+     * @param phoneNumbers  电话号码
+     * @param templateId    自定义模版的ID，用于区分多个模版，该temlateId不是阿里短信的模版id。而是自己在配置文件中配置的、与阿里模版id对应的key值
+     * @param templateParam 短信模版中涉及的参数
+     * @return 自定义短信响应实体 SmsResponse {@link SmsResponse}
+     */
+    public SmsResponse send(List<String> phoneNumbers, String templateId, Map<String, Object> templateParam) {
+        String phoneNumberString = StringUtils.join(phoneNumbers, ",");
+        return this.send(phoneNumberString, templateId, templateParam);
+    }
 
-	public SmsResponse send(String phoneNumbers, String templateId, Map<String, Object> templateParam) {
-		String templateParamString = JSON.toJSONString(templateParam);
-		String templateCode = aliyunProperties.getSms().getTemplates().get(templateId);
-		if (StringUtils.isBlank(templateCode)) {
-			log.error("[Eurynome] |- Can not found templateId in Aliyun sms templates property, please check!");
-		}
-		return  this.send(phoneNumbers, templateCode, templateParamString);
-	}
+    public SmsResponse send(String phoneNumbers, String templateId, Map<String, Object> templateParam) {
+        String templateParamString = JSON.toJSONString(templateParam);
+        String templateCode = aliyunProperties.getSms().getTemplates().get(templateId);
+        if (StringUtils.isBlank(templateCode)) {
+            log.error("[Eurynome] |- Can not found templateId in Aliyun sms templates property, please check!");
+        }
+        return this.send(phoneNumbers, templateCode, templateParamString);
+    }
 
-	private SmsResponse send(String phoneNumbers, String templateCode, String templateParam) {
-		SendSmsRequest sendSmsRequest = new SendSmsRequest();
-		sendSmsRequest.setPhoneNumbers(phoneNumbers);
-		sendSmsRequest.setSignName(aliyunProperties.getSms().getSignName());
-		sendSmsRequest.setTemplateCode(templateCode);
-		sendSmsRequest.setTemplateParam(templateParam);
-		return this.send(sendSmsRequest);
-	}
+    private SmsResponse send(String phoneNumbers, String templateCode, String templateParam) {
+        SendSmsRequest sendSmsRequest = new SendSmsRequest();
+        sendSmsRequest.setPhoneNumbers(phoneNumbers);
+        sendSmsRequest.setSignName(aliyunProperties.getSms().getSignName());
+        sendSmsRequest.setTemplateCode(templateCode);
+        sendSmsRequest.setTemplateParam(templateParam);
+        return this.send(sendSmsRequest);
+    }
 
 
-	private SmsResponse send(SendSmsRequest sendSmsRequest) {
-		CommonRequest commonRequest = new CommonRequest();
-		commonRequest.setSysMethod(MethodType.POST);
-		commonRequest.setSysDomain(aliyunProperties.getSms().getDomain());
-		commonRequest.setSysVersion(aliyunProperties.getSms().getVersion());
-		commonRequest.setSysAction(sendSmsRequest.getAction());
-		sendSmsRequest.getParameters().forEach(commonRequest::putQueryParameter);
+    private SmsResponse send(SendSmsRequest sendSmsRequest) {
+        CommonRequest commonRequest = new CommonRequest();
+        commonRequest.setSysMethod(MethodType.POST);
+        commonRequest.setSysDomain(aliyunProperties.getSms().getDomain());
+        commonRequest.setSysVersion(aliyunProperties.getSms().getVersion());
+        commonRequest.setSysAction(sendSmsRequest.getAction());
+        sendSmsRequest.getParameters().forEach(commonRequest::putQueryParameter);
 
-		try {
-			CommonResponse commonResponse = iAcsClient.getCommonResponse(commonRequest);
-			if (ObjectUtils.isNotEmpty(commonResponse) && commonResponse.getHttpStatus() == 200) {
-				SmsResponse smsResponse = JSON.parseObject(commonResponse.getData(), SmsResponse.class);
-				log.debug("[Eurynome] |- Aliyun send message result success. result is: {}", smsResponse.toString());
-				return smsResponse;
-			} else {
-				log.error("[Eurynome] |- Aliyun send message result convert to entity error!");
-				return null;
-			}
-		} catch (ClientException e) {
-			log.error("[Eurynome] |- Aliyun send message catch ClientException: {}", e.getMessage());
-			return null;
-		}
-	}
+        try {
+            CommonResponse commonResponse = iAcsClient.getCommonResponse(commonRequest);
+            if (ObjectUtils.isNotEmpty(commonResponse) && commonResponse.getHttpStatus() == 200) {
+                SmsResponse smsResponse = JSON.parseObject(commonResponse.getData(), SmsResponse.class);
+                log.debug("[Eurynome] |- Aliyun send message result success. result is: {}", smsResponse.toString());
+                return smsResponse;
+            } else {
+                log.error("[Eurynome] |- Aliyun send message result convert to entity error!");
+                return null;
+            }
+        } catch (ClientException e) {
+            log.error("[Eurynome] |- Aliyun send message catch ClientException: {}", e.getMessage());
+            return null;
+        }
+    }
 }
