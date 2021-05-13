d11c551a5c0bce7ac29fd4be66cb37aef85dd543
Felix <pointer_v@qq.com>
2021-05-13 15:30:18 +0800

    update

--- packages/eurynome-cloud-security/src/main/java/cn/herodotus/eurynome/security/authentication/token/HerodotusJwtTokenEnhancer.java
+++ packages/eurynome-cloud-security/src/main/java/cn/herodotus/eurynome/security/authentication/token/HerodotusJwtTokenEnhancer.java
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2019-2021 Gengwei Zheng(herodotus@aliyun.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Project Name: eurynome-cloud
+ * Module Name: eurynome-cloud-security
+ * File Name: HerodotusJwtTokenEnhancer.java
+ * Author: gengwei.zheng
+ * Date: 2021/05/07 11:28:07
+ */
+
 package cn.herodotus.eurynome.security.authentication.token;
 
 import cn.herodotus.eurynome.common.constants.SecurityConstants;
