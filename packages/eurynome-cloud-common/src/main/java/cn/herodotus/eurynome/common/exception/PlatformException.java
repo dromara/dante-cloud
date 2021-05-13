91242f5bed2224b90224dd0246328d06fce8e640
Felix <pointer_v@qq.com>
2021-05-13 14:31:01 +0800

    update

--- packages/eurynome-cloud-common/src/main/java/cn/herodotus/eurynome/common/exception/PlatformException.java
+++ packages/eurynome-cloud-common/src/main/java/cn/herodotus/eurynome/common/exception/PlatformException.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2019-2019 the original author or authors.
+ * Copyright (c) 2019-2021 Gengwei Zheng(herodotus@aliyun.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,13 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *
- * Project Name: luban-cloud
- * Module Name: luban-cloud-component-common
+ * Project Name: eurynome-cloud
+ * Module Name: eurynome-cloud-common
  * File Name: PlatformException.java
  * Author: gengwei.zheng
- * Date: 2019/11/8 下午4:12
- * LastModified: 2019/11/8 下午4:12
+ * Date: 2021/05/07 11:28:07
  */
 
 package cn.herodotus.eurynome.common.exception;
