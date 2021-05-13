274431c83e75de59cf98f12a562b9981f2965514
Felix <pointer_v@qq.com>
2021-05-13 15:50:27 +0800

    update

--- services/eurynome-cloud-upms-ability/src/main/java/cn/herodotus/eurynome/upms/ability/processor/AbstractSecurityMetadataStorage.java
+++ services/eurynome-cloud-upms-ability/src/main/java/cn/herodotus/eurynome/upms/ability/processor/AbstractSecurityMetadataStorage.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2019-2019 the original author or authors.
+ * Copyright (c) 2019-2021 Gengwei Zheng(herodotus@aliyun.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,13 +13,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  *
- *
- * Project Name: luban-cloud
- * Module Name: luban-cloud-upms-logic
+ * Project Name: eurynome-cloud
+ * Module Name: eurynome-cloud-upms-ability
  * File Name: AbstractSecurityMetadataStorage.java
  * Author: gengwei.zheng
- * Date: 2019/11/21 下午12:38
- * LastModified: 2019/11/21 下午12:38
+ * Date: 2021/05/07 11:46:07
  */
 
 package cn.herodotus.eurynome.upms.ability.processor;
