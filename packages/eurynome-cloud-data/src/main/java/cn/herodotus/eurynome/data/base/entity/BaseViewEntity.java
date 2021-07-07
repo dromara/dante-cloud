cbac15e4057b00acbe319689bb6822d724381879
herodotus <pointer_v@qq.com>
2021-07-07 16:40:12 +0800

    🏁 v2.5.2.37 1. 抽象实体共性属性，拓展基础实体，以支持JPA视图类的ORM映射。

--- packages/eurynome-cloud-data/src/main/java/cn/herodotus/eurynome/data/base/entity/BaseViewEntity.java
+++ packages/eurynome-cloud-data/src/main/java/cn/herodotus/eurynome/data/base/entity/BaseViewEntity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019-2021 Gengwei Zheng(herodotus@aliyun.com)
+ * Copyright (c) 2019-2021 Gengwei Zheng (herodotus@aliyun.com)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,26 +15,23 @@
  *
  * Project Name: eurynome-cloud
  * Module Name: eurynome-cloud-data
- * File Name: BaseEntity.java
+ * File Name: BaseViewEntity.java
  * Author: gengwei.zheng
- * Date: 2021/05/07 11:28:07
+ * Date: 2021/07/07 16:36:07
  */
 
 package cn.herodotus.eurynome.data.base.entity;
 
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 
 /**
- * <p> Description : 实体通用属性 </p>
+ * <p>Description: 视图类型实体基础类 </p>
+ * <p>
+ * 视图只能支持读取，而不支持新增、修改、删除等操作。因此，为了产生比必要的隐患，规避@EntityListeners(AuditingEntityListener.class)注解可能会出现的修改操作。
  *
  * @author : gengwei.zheng
- * @date : 2020/4/29 16:09
+ * @date : 2021/7/7 16:36
  */
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
-public abstract class BaseEntity extends CommonEntity {
-
+public abstract class BaseViewEntity extends CommonEntity {
 }
