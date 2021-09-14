eb5da84669ea79c089aeeae9a7cc410b12e1857b
herodotus <pointer_v@qq.com>
2021-09-14 21:48:37 +0800

    fix: v2.5.4.131
    1. 去掉无用的拦截

--- packages/eurynome-cloud-oauth-starter/src/main/java/cn/herodotus/eurynome/oauth/autoconfigure/WebSecurityAutoConfiguration.java
+++ packages/eurynome-cloud-oauth-starter/src/main/java/cn/herodotus/eurynome/oauth/autoconfigure/WebSecurityAutoConfiguration.java
@@ -36,6 +36,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
