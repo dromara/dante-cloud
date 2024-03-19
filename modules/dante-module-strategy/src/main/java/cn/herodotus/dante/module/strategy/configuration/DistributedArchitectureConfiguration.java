0f9d8d9b7b0dfca6284efb8231fdefc0bd76cb6a
码匠君 <pointer_v@qq.com>
2024-03-19 22:10:43 +0800

    使用 GRPC 方式远程调用接口模式验证完成

--- modules/dante-module-strategy/src/main/java/cn/herodotus/dante/module/strategy/configuration/DistributedArchitectureConfiguration.java
+++ modules/dante-module-strategy/src/main/java/cn/herodotus/dante/module/strategy/configuration/DistributedArchitectureConfiguration.java
@@ -46,6 +46,7 @@ import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -91,6 +92,9 @@ public class DistributedArchitectureConfiguration {
 
     @Configuration(proxyBeanMethods = false)
     @ConditionalOnRemoteDataAccess
+    @ConditionalOnMissingClass({
+            "net.devh.boot.grpc.client.autoconfigure.GrpcClientAutoConfiguration"
+    })
     @EnableFeignClients(basePackages = {"cn.herodotus.dante.module.strategy.feign"})
     static class DataAccessStrategyRemoteConfiguration {
 
