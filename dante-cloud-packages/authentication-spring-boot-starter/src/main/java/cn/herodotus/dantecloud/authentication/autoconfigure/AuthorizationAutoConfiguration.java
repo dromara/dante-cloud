8fafb239188820c5b3d909b075133f6d58a8d1b1
码匠君 <pointer_v@qq.com>
2026-09-06 12:23:55 +0800

    [新增] 认证服务器配置新增 client_id_metadata_document_supported 设置标识

--- dante-cloud-packages/authentication-spring-boot-starter/src/main/java/cn/herodotus/dantecloud/authentication/autoconfigure/AuthorizationAutoConfiguration.java
+++ dante-cloud-packages/authentication-spring-boot-starter/src/main/java/cn/herodotus/dantecloud/authentication/autoconfigure/AuthorizationAutoConfiguration.java
@@ -25,6 +25,7 @@
 
 package cn.herodotus.dantecloud.authentication.autoconfigure;
 
+import cn.herodotus.dante.core.constant.SystemConstants;
 import cn.herodotus.dante.oauth2.authentication.configurer.OAuth2AuthenticationConfigurerManager;
 import cn.herodotus.dante.oauth2.authentication.configurer.OAuth2AuthenticationProviderConfigurer;
 import cn.herodotus.dante.oauth2.authentication.customizer.OAuth2AuthorizationServerConfigurerCustomizer;
@@ -125,7 +126,7 @@ public class AuthorizationAutoConfiguration {
     }
 
     @Bean
-    public AuthorizationServerSettings authorizationServerSettings(EndpointProperties endpointProperties) {
+    public AuthorizationServerSettings authorizationServerSettings(EndpointProperties endpointProperties, OAuth2AuthenticationProperties oauth2AuthenticationProperties) {
         return AuthorizationServerSettings.builder()
                 .issuer(endpointProperties.getIssuerUri())
                 .authorizationEndpoint(endpointProperties.getAuthorizationEndpoint())
@@ -139,6 +140,7 @@ public class AuthorizationAutoConfiguration {
                 .oidcLogoutEndpoint(endpointProperties.getOidcLogoutEndpoint())
                 .oidcUserInfoEndpoint(endpointProperties.getOidcUserInfoEndpoint())
                 .oidcClientRegistrationEndpoint(endpointProperties.getOidcClientRegistrationEndpoint())
+                .setting(SystemConstants.PARAMETER__CLIENT_ID_METADATA_DOCUMENT_SUPPORTED, oauth2AuthenticationProperties.getClientIdMetadataDocument().getEnabled())
                 .build();
     }
 }
